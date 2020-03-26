b6ea3e186c7154819dbae51b0e4da411389f0ce6
raghavjpr <raghavjpr@gmail.com>
2020-03-26 21:02:30 +0530

    trying adding message and file

---
+++ try.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--){
+        cout << "Trying Git : " << t << endl;
+    }
+    return 0;
+}
